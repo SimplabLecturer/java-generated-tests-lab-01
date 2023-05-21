a0885ccbbe41e6dfe992ba2c6f74fe7ab37ee5ae
Yurii Matora <ymatora@itsmartflex.com>
2023-05-22 00:40:11 +0200

    Initial commit for task repo

---
+++ src/main/java/com/simplab/test/generated/TestTasks.java
@@ -0,0 +1,16 @@
+package com.simplab.test.generated;
+
+public class TestTasks {
+
+    public int fibonacci(int number) {
+        return 0;
+    }
+
+    public boolean isPrimeNumber(int number) {
+        return false;
+    }
+
+    public String reverse(String source) {
+        return "";
+    }
+}
